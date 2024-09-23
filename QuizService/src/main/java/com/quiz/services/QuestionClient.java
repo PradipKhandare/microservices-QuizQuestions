e8b0288c9baf1c33d586c57c8b8814ace8c498b1
PradipKhandare <pradipkhandare111@gmail.com>
2024-09-23 13:34:14 +0530

    initial commit

---
+++ QuizService/src/main/java/com/quiz/services/QuestionClient.java
@@ -0,0 +1,18 @@
+package com.quiz.services;
+import com.quiz.entities.Question;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+//@FeignClient(
+//        url = "http://localhost:8082",
+//        value = "question-client"
+//)
+@FeignClient(name = "QUESTIONSERVICE")
+public interface QuestionClient {
+
+    @GetMapping("/question/quiz/{id}")
+    List<Question> getQuestionOfQuiz(@PathVariable Long id);
+}
